6690601d32457d027be364fd378c10af2e735a29
Fırat Şakar <frtskr0@gmail.com>
2025-02-11 22:13:44 +0300

    `catalog-api`: create docker compose files and get products endpoint and handle classes

---
+++ src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductsEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.GetProduct;
+
+public record GetProductResponse(IEnumerable<Product> Products);
+
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet(
+                "/products",
+                async (ISender sender) =>
+                {
+                    var result = await sender.Send(new GetProductsQuery());
+                    var response = result.Adapt<GetProductsEndpoint>();
+
+                    return Results.Ok(response);
+                }
+            )
+            .WithName("GetProducts")
+            .Produces<GetProductsEndpoint>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get Products");
+    }
+}
