0fa9dbb208744b25b137e21e71f81f4366281461
Fırat Şakar <frtskr0@gmail.com>
2025-07-19 23:13:25 +0300

    create DeleteProduct endpoint and handler classes and implemented

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductResponse(bool IsSuccess);
+public class DeleteProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/products/{id:guid}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteProductCommand(id));
+            var response = result.Adapt<DeleteProductResponse>();
+
+            return Results.Ok(response);
+        })
+            .WithName("DeleteProduct")
+            .WithSummary("Delete a product by ID")
+            .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status500InternalServerError)
+            .WithDescription("Delete a product by ID");
+    }
+}
+
