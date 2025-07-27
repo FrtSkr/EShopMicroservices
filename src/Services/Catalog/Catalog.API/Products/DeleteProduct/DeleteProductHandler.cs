a69b0e217c2665789f5e3ff2a4b9496f39b89db9
Fırat Şakar <frtskr0@gmail.com>
2025-07-27 21:23:06 +0300

    add delete product command validator

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -4,6 +4,13 @@ namespace Catalog.API.Products.DeleteProduct;
 public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
 public record DeleteProductResult(bool IsSuccess);
 
+public class DeleteProdeuctCommandValidator : AbstractValidator<DeleteProductCommand>
+{
+    public DeleteProdeuctCommandValidator()
+    {
+        RuleFor(r => r.Id).NotEmpty().WithMessage("Id is required.");
+    }
+}
 internal class DeleteProductHandler(IDocumentSession session, ILogger<DeleteProductHandler> logger) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
 {
     public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
