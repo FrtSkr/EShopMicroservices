bc2f952941b2acc221cf8d8ed594b04e63e7a766
Fırat Şakar <frtskr0@gmail.com>
2025-10-29 15:29:22 +0300

    add pagination when get products with marten IPagedList

--- src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductsQueryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductsQueryHandler.cs
@@ -1,10 +1,10 @@
-﻿using Marten.Linq.QueryHandlers;
+﻿using Marten.Pagination;
 
 namespace Catalog.API.Products.GetProduct;
 
-public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
 
-public record GetProductsResult(IEnumerable<Product> Products);
+public record GetProductsResult(IPagedList<Product> Products);
 
 internal class GetProductsQueryHandler(
     IDocumentSession session
@@ -15,7 +15,7 @@ internal class GetProductsQueryHandler(
         CancellationToken cancellationToken
     )
     {
-        var products = await session.Query<Product>().ToListAsync();
+        var products = await session.Query<Product>().ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10, cancellationToken);
 
         return new GetProductsResult(products);
     }
